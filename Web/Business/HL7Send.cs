945a88a47fcb865d028e3c337ae27ddc4e1d772a
Phil Campbell <philoushka@gmail.com>
2013-11-08 09:05:36 -0800

    hardcode \r\n instead of Environment.NewLine; remove/sort usings.

--- Web/Business/HL7Send.cs
+++ Web/Business/HL7Send.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using HL7_TCP.Web.Models;
+﻿using HL7_TCP.Web.Models;
+using System;
 using System.Diagnostics;
 
 namespace HL7_TCP.Web
@@ -21,16 +18,15 @@ namespace HL7_TCP.Web
 
                 if (results.ExceptionDuringSend.IsNullOrEmpty())
                 {
-                    return "Successfully sent {0} message{2} to {1}.{4}Total time taken: {3}".FormatWith(model.NumMessages,
+                    return "Successfully sent {0} message{2} to {1}.\r\nTotal time taken: {3}".FormatWith(model.NumMessages,
                                                                                                          model.DestinationDetails,
                                                                                                          (results.NumberMsgsSent > 1) ? "s" : "",
-                                                                                                         results.TimeElapsed.ToReadableString(),
-                                                                                                         Environment.NewLine);
+                                                                                                         results.TimeElapsed.ToReadableString());
                 }
                 else
                 {
-                    return "We had a problem sending message {0} to {1}.   {2}".FormatWith(results.NumberMsgsSent + 1, 
-                                                                                            model.DestinationDetails, 
+                    return "We had a problem sending message {0} to {1}.   {2}".FormatWith(results.NumberMsgsSent + 1,
+                                                                                            model.DestinationDetails,
                                                                                             results.ExceptionDuringSend);
                 }
             }
@@ -59,8 +55,5 @@ namespace HL7_TCP.Web
                 return new TcpSendResults { TimeElapsed = s.Elapsed, NumberMsgsSent = sendMsgCounter, ExceptionDuringSend = e.Message };
             }
         }
-
     }
-
-
 }
