4d2c490d1505d29e2d3183ec904ac93527b4be1e
Phil Campbell <philoushka@gmail.com>
2013-11-08 09:33:02 -0800

    remove asp.net default css items. move css and js to their own files. style form on index to look decent.

---
+++ Web/Scripts/HL7Send.js
@@ -0,0 +1,23 @@
+﻿   $(function () {
+        var msg = "@ViewBag.Message";
+        if (msg) {
+            $("#msg").html(msg);
+        }
+        $("#numMsgsToSend").ForceNumericOnly();
+        $("#destinationPort").ForceNumericOnly();
+
+    });
+
+    jQuery.fn.ForceNumericOnly = function () {
+        return this.each(function () {
+            $(this).keydown(function (e) {
+                var key = e.charCode || e.keyCode || 0;
+                // allow backspace, tab, delete, arrows, numbers and keypad numbers ONLY, home, and end.
+                return (
+                    key == 8 || key == 9 || key == 190 ||
+                    (key >= 35 && key <= 40) ||
+                    (key >= 48 && key <= 57) ||
+                    (key >= 96 && key <= 105));
+            });
+        });
+    };
